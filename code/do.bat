a18a7f629e847a0af91557495cd4667962885c13
ambergon <sc.foxgon@gmail.com>
2024-06-17 22:15:28 +0900

    [fix]
    static引数忘れ

--- code/do.bat
+++ code/do.bat
@@ -2,7 +2,7 @@
 
 
 del /q ..\main.dll
-g++ -shared -o ../main.dll main.cpp 
+g++ -shared -o ../main.dll main.cpp -static
 REM g++ -shared -o ../main.dll main.cpp -static -lsqlite3
 @rem g++ -pthread -shared -o main.dll main.cpp -I ./include  liblua54.a -g
 @rem g++ -shared -o main.dll main.cpp -I ./include  liblua54.a -g
